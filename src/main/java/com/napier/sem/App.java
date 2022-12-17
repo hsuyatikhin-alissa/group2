365aaa3336df0c4a595b4a8886147d13d5da9496
hsuyatikhin-alissa <102844238+hsuyatikhin-alissa@users.noreply.github.com>
2022-12-17 19:05:43 +0630

    Update App.java

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -2,58 +2,82 @@ package com.napier.sem;
 
 import java.sql.*;
 
-import java.util.ArrayList;
-
 public class App
 {
-    public static void main(String[] args)
-    {
+    /**
+     * Connection to MySQL database.
+     */
+    private Connection con = null;
 
-        DBConnect dbcon = new DBConnect();
-        // Connect to database
-        if(args.length < 1){
-            dbcon.connect("localhost:33060", 30000);
-        }else{
-            dbcon.connect(args[0], Integer.parseInt(args[1]));
+    /**
+     * Connect to the MySQL database.
+     */
+    public void connect()
+    {
+        try
+        {
+            // Load Database driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }
+        catch (ClassNotFoundException e)
+        {
+            System.out.println("Could not load SQL driver");
+            System.exit(-1);
         }
 
-        /**
-         * 1. All the country in the world organised by largest population to smallest.
-         */
-        // Create new Application
-        AllCountriesInWorld ctrwo = new AllCountriesInWorld();
-        // Connect to database
-        ctrwo.setCon(dbcon.getCon());
-        ArrayList<Country> cctrwo = ctrwo.getAllCountries();
-        // Extract country information
-        ctrwo.printCountries(cctrwo);
-
-        /**
-         * 11. All the cities in a district organised by largest population to smallest.
-         */
-        AllCitiesInADistrict cdist = new AllCitiesInADistrict();
-        cdist.setCon(dbcon.getCon());
-        ArrayList<City> ccdist= cdist.getAllCities();
-        cdist.printCities(ccdist);
-
-
-        /**
-         * 15. The top N populated cities in a country where N is provided by the user.
-         */
-        TopNPopulatedCitiesInACountry tcntry = new TopNPopulatedCitiesInACountry();
-        tcntry.setCon(dbcon.getCon());
-        ArrayList<City> tccntry = tcntry.getAllCities();
-        tcntry.printCities(tccntry);
-
-
-        /**
-         * 16. The top N populated cities in a district where N is provided by the user.
-         *
-         */
-        TopNPopulatedCitiesInADistrict tcdist = new TopNPopulatedCitiesInADistrict();
-        tcdist.setCon(dbcon.getCon());
-        ArrayList<City> tccdist = tcdist.getAllCities();
-        tcdist.printCities(tccdist);
+        int retries = 10;
+        for (int i = 0; i < retries; ++i)
+        {
+            System.out.println("Connecting to database...");
+            try
+            {
+                // Wait a bit for db to start
+                Thread.sleep(30000);
+                // Connect to database
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
+                System.out.println("Successfully connected");
+                break;
+            }
+            catch (SQLException sqle)
+            {
+                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+                System.out.println(sqle.getMessage());
+            }
+            catch (InterruptedException ie)
+            {
+                System.out.println("Thread interrupted? Should not happen.");
+            }
+        }
     }
 
+    /**
+     * Disconnect from the MySQL database.
+     */
+    public void disconnect()
+    {
+        if (con != null)
+        {
+            try
+            {
+                // Close connection
+                con.close();
+            }
+            catch (Exception e)
+            {
+                System.out.println("Error closing connection to database");
+            }
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        // Create new Application
+        App a = new App();
+
+        // Connect to database
+        a.connect();
+
+        // Disconnect from database
+        a.disconnect();
+    }
 }
