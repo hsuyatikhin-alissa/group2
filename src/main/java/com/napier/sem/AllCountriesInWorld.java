a85b64d8bb080702ddfdf23ac63908fc05f91431
hsuyatikhin-alissa <102844238+hsuyatikhin-alissa@users.noreply.github.com>
2022-12-22 20:12:51 +0630

    Update App.java and modified issues 1, 11, 15 & 16 by HYK

--- src/main/java/com/napier/sem/AllCountriesInWorld.java
+++ src/main/java/com/napier/sem/AllCountriesInWorld.java
@@ -7,66 +7,10 @@ public class AllCountriesInWorld {
     /**
      * Connection to MySQL database.
      */
-    private Connection con = null;
+    private Connection con;
 
-    /**
-     * Connect to the MySQL database.
-     */
-    public void connect()
-    {
-        try
-        {
-            // Load Database driver
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        }
-        catch (ClassNotFoundException e)
-        {
-            System.out.println("Could not load SQL driver");
-            System.exit(-1);
-        }
-
-        int retries = 10;
-        for (int i = 0; i < retries; ++i)
-        {
-            System.out.println("Connecting to database...");
-            try
-            {
-                // Wait a bit for db to start
-                Thread.sleep(30000);
-                // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
-                System.out.println("Successfully connected");
-                break;
-            }
-            catch (SQLException sqle)
-            {
-                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
-                System.out.println(sqle.getMessage());
-            }
-            catch (InterruptedException ie)
-            {
-                System.out.println("Thread interrupted? Should not happen.");
-            }
-        }
-    }
-
-    /**
-     * Disconnect from the MySQL database.
-     */
-    public void disconnect()
-    {
-        if (con != null)
-        {
-            try
-            {
-                // Close connection
-                con.close();
-            }
-            catch (Exception e)
-            {
-                System.out.println("Error closing connection to database");
-            }
-        }
+    public void setCon(Connection con) {
+        this.con = con;
     }
 
     /**
@@ -78,7 +22,8 @@ public class AllCountriesInWorld {
             Statement stmt = con.createStatement();
             // Create string for SQL statement
             String strSelect =
-                    "SELECT country.Code, country.Name, country.Continent, country.Region, country.Population, country.Capital FROM country ORDER BY country.Population DESC";
+                    "SELECT country.Code, country.Name, country.Continent, country.Region, "
+                        + "country.Population, country.Capital FROM country ORDER BY country.Population DESC ";
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
             // Extract countries information
@@ -106,7 +51,8 @@ public class AllCountriesInWorld {
      */
     public void printCountries(ArrayList<Country> countries) {
         // Print header
-        System.out.println("All the countries in the world organised by largest population to smallest");
+        System.out.println("1. All the countries in the world organised by largest population to smallest");
+        System.out.println();
         System.out.println(String.format("%-15s %-50s %-20s %-35s %-20s %s", "Code", "Name", "Continent", "Region", "Population", "Capital"));
         // Loop over all employees in the list
         for (Country cntry : countries) {
@@ -114,6 +60,7 @@ public class AllCountriesInWorld {
                     String.format("%-15s %-50s %-20s %-35s %-20s %s", cntry.getCode(), cntry.getName(), cntry.getContinent(), cntry.getRegion(), cntry.getPopulation(), cntry.getCapital());
             System.out.println(cntry_string);
         }
+        System.out.println();
     }
 
 }
