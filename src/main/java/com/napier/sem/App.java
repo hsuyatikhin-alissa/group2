5850ca1ca01421ef1b04c7122926caec39e8ce27
hsuyatikhin-alissa <102844238+hsuyatikhin-alissa@users.noreply.github.com>
2022-12-22 20:12:51 +0630

    Update App.java and modified issues 1, 11, 15 & 16 by HYK

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -6,63 +6,49 @@ import java.util.ArrayList;
 
 public class App
 {
-
-
     public static void main(String[] args)
     {
-        /**
-         * All the country in the world organised by largest population to smallest.
-         */
-//        // Create new Application
-//        AllCountriesInWorld ctrwo = new AllCountriesInWorld();
-//        // Connect to database
-//        ctrwo.connect();
-//        ArrayList<Country> cctrwo = ctrwo.getAllCountries();
-//        // Extract country information
-//        ctrwo.printCountries(cctrwo);
-//        // Disconnect from database
-//        ctrwo.disconnect();    
+        DBConnect dbcon = new DBConnect();
+        // Connect to database
+        dbcon.connect();
 
         /**
-         * All the cities in a district organised by largest population to smallest.
-         */
-//        //Create new Application
-//        AllCitiesInADistrict cdist = new AllCitiesInADistrict();
-//        // Connect to database
-//        cdist.connect();
-//        ArrayList<City> ccdist= cdist.getAllCities();
-//        // Extract cities information
-//        cdist.printCities(ccdist);
-//        // Disconnect from database
-//        cdist.disconnect();
-
-
-        /**
-         * The top N populated cities in a country where N is provided by the user.
-         */
-//        // Create new Application
-//        TopNPopulatedCitiesInACountry tcntry = new TopNPopulatedCitiesInACountry();
-//        // Connect to database
-//        tcntry.connect();
-//        ArrayList<City> tccntry = tcntry.getAllCities();
-//        // Extract cities information
-//        tcntry.printCities(tccntry);
-//        // Disconnect from database
-//        tcntry.disconnect();
-
-        /**
-         * The top N populated cities in a district where N is provided by the user.
-         *
+         * 1. All the country in the world organised by largest population to smallest.
          */
         // Create new Application
-        TopNPopulatedCitiesInADistrict tcdist = new TopNPopulatedCitiesInADistrict();
+        AllCountriesInWorld ctrwo = new AllCountriesInWorld();
         // Connect to database
-        tcdist.connect();
+        ctrwo.setCon(dbcon.getCon());
+        ArrayList<Country> cctrwo = ctrwo.getAllCountries();
+        // Extract country information
+        ctrwo.printCountries(cctrwo);
+
+        /**
+         * 11. All the cities in a district organised by largest population to smallest.
+         */
+        AllCitiesInADistrict cdist = new AllCitiesInADistrict();
+        cdist.setCon(dbcon.getCon());
+        ArrayList<City> ccdist= cdist.getAllCities();
+        cdist.printCities(ccdist);
+
+
+        /**
+         * 15. The top N populated cities in a country where N is provided by the user.
+         */
+        TopNPopulatedCitiesInACountry tcntry = new TopNPopulatedCitiesInACountry();
+        tcntry.setCon(dbcon.getCon());
+        ArrayList<City> tccntry = tcntry.getAllCities();
+        tcntry.printCities(tccntry);
+
+
+        /**
+         * 16. The top N populated cities in a district where N is provided by the user.
+         *
+         */
+        TopNPopulatedCitiesInADistrict tcdist = new TopNPopulatedCitiesInADistrict();
+        tcdist.setCon(dbcon.getCon());
         ArrayList<City> tccdist = tcdist.getAllCities();
-        // Extract cities information
         tcdist.printCities(tccdist);
-        // Disconnect from database
-        tcdist.disconnect();
     }
 
 }
