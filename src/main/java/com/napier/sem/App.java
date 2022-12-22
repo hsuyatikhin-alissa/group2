9fe7eec7f89df4463d978940306fec11da3f47b7
Aye Thandar Phyo <40524297@live.napier.ac.uk>
2022-12-22 19:50:42 +0630

    Completed Issues 8, 9, 13 and 14 (Updated).

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -2,82 +2,95 @@ package com.napier.sem;
 
 import java.sql.*;
 
+import java.util.ArrayList;
+
 public class App
 {
-    /**
-     * Connection to MySQL database.
-     */
-    private Connection con = null;
 
-    /**
-     * Connect to the MySQL database.
-     */
-    public void connect()
-    {
-        try
-        {
-            // Load Database driver
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        }
-        catch (ClassNotFoundException e)
-        {
-            System.out.println("Could not load SQL driver");
-            System.exit(-1);
-        }
-
-        int retries = 10;
-        for (int i = 0; i < retries; ++i)
-        {
-            System.out.println("Connecting to database...");
-            try
-            {
-                // Wait a bit for db to start
-                Thread.sleep(30000);
-                // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
-                System.out.println("Successfully connected");
-                break;
-            }
-            catch (SQLException sqle)
-            {
-                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
-                System.out.println(sqle.getMessage());
-            }
-            catch (InterruptedException ie)
-            {
-                System.out.println("Thread interrupted? Should not happen.");
-            }
-        }
-    }
-
-    /**
-     * Disconnect from the MySQL database.
-     */
-    public void disconnect()
-    {
-        if (con != null)
-        {
-            try
-            {
-                // Close connection
-                con.close();
-            }
-            catch (Exception e)
-            {
-                System.out.println("Error closing connection to database");
-            }
-        }
-    }
 
     public static void main(String[] args)
     {
-        // Create new Application
-        App a = new App();
+
+/*All the cities in a continent organised by largest population to smallest.*/
+
+        // Create an object to call AllCitiesInAContinent Class
+        AllCitiesInAContinent ccon = new AllCitiesInAContinent();
 
         // Connect to database
-        a.connect();
+        ccon.connect();
+
+        // Get all cities information in a continent
+        ArrayList<City> cities = ccon.getAllCitiesInAContinent();
+
+        // Extract all cities information in a continent
+        ccon.printCitiesInAContinent(cities);
 
         // Disconnect from database
-        a.disconnect();
+        ccon.disconnect();
+
+
+
+
+
+/*All the cities in a region organised by largest population to smallest.*/
+
+//        // Create an object to call AllCitiesInARegion Class
+//        AllCitiesInARegion creg = new AllCitiesInARegion();
+//
+//        // Connect to database
+//        creg.connect();
+//
+//        // Get all cities information in a region
+//        ArrayList<City> ccreg = creg.getAllCitiesInARegion();
+//
+//        // Extract all cities information in a region
+//        creg.printCitiesInARegion(ccreg);
+//
+//        // Disconnect from database
+//        creg.disconnect();
+
+
+
+
+
+/*The top N populated cities in a continent where N is provided by the user.*/
+
+//        // Create an object to call TopNPopulatedCitiesInAContinent Class
+//        TopNPopulatedCitiesInAContinent tcon = new TopNPopulatedCitiesInAContinent();
+//
+//        // Connect to database
+//        tcon.connect();
+//
+//        //Get top N populated cities information in a continent
+//        ArrayList<City> tccon = tcon.getAllCities();
+//
+//        // Extract top N populated cities information in a continent
+//        tcon.printCities(tccon);
+//
+//        // Disconnect from database
+//        tcon.disconnect();
+
+
+
+
+
+/* The top N populated cities in a region where N is provided by the user.*/
+
+//        // Create an object to call TopNPopulatedCitiesInARegion Class
+//        TopNPopulatedCitiesInARegion treg = new TopNPopulatedCitiesInARegion();
+//
+//        // Connect to database
+//        treg.connect();
+//
+//        //Get top N populated cities information in a region
+//        ArrayList<City> tccreg = treg.getAllCities();
+//
+//        // Extract top N populated cities information in a region
+//        treg.printCities(tccreg);
+//
+//        // Disconnect from database
+//        treg.disconnect();
+
     }
+
 }
---
+++ src/main/java/com/napier/sem/TopNPopulatedCitiesInARegion.java
@@ -0,0 +1,140 @@
+package com.napier.sem;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class TopNPopulatedCitiesInARegion {
+
+    /**
+     * Connection to MySQL database.
+     */
+    private Connection con = null;
+
+    /**
+     * Connect to the MySQL database.
+     */
+    public void connect()
+    {
+        try
+        {
+            // Load Database driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }
+        catch (ClassNotFoundException e)
+        {
+            System.out.println("Could not load SQL driver");
+            System.exit(-1);
+        }
+
+        int retries = 10;
+        for (int i = 0; i < retries; ++i)
+        {
+            System.out.println("Connecting to database...");
+            try
+            {
+                // Wait a bit for db to start
+                Thread.sleep(30000);
+                // Connect to database
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
+                System.out.println("Successfully connected");
+                break;
+            }
+            catch (SQLException sqle)
+            {
+                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+                System.out.println(sqle.getMessage());
+            }
+            catch (InterruptedException ie)
+            {
+                System.out.println("Thread interrupted? Should not happen.");
+            }
+        }
+    }
+
+    /**
+     * Disconnect from the MySQL database.
+     */
+    public void disconnect()
+    {
+        if (con != null)
+        {
+            try
+            {
+                // Close connection
+                con.close();
+            }
+            catch (Exception e)
+            {
+                System.out.println("Error closing connection to database");
+            }
+        }
+    }
+
+
+    /**
+     * Gets the top N populated cities in a region where N is provided by the user.
+     * @return A list of the top N populated cities in a region where N is provided by the user.
+     */
+    public ArrayList<City> getAllCities()
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT city.Name, country.Name, country.Region, city.District, city.Population "
+                            + "FROM city "
+                            + "JOIN country country "
+                            + "ON country.Code = city.CountryCode "
+                            + "WHERE country.Region = 'Eastern Asia' "
+                            + "ORDER BY city.Population DESC "
+                            + "LIMIT 3 ";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Extract the top N populated cities in a region where N is provided by the user.
+            ArrayList<City> cities = new ArrayList<City>();
+            while (rset.next())
+            {
+                City cty = new City();
+                Country cntry = new Country();
+                cty.setName(rset.getString(1));
+                cntry.setName(rset.getString(2));
+                cntry.setRegion(rset.getString(3));
+                cty.setDistrict(rset.getString(4));
+                cty.setPopulation(rset.getInt(5));
+                cty.setCountry(cntry);
+                cities.add(cty);
+            }
+            return cities;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get city details");
+            return null;
+        }
+    }
+
+
+    /**
+     * Prints a list of the top N populated cities in a region where N is provided by the user.
+     * @param cities The top N populated cities in a region where N is provided by the user.
+     */
+    public void printCities(ArrayList<City> cities)
+    {
+        // Print header
+        System.out.println(String.format("%-30s %-30s %-30s %-20s %s", "Name", "Country", "Region", "District", "Population"));
+        // Loop over all cities in the list
+        for (City cty : cities)
+        {
+            String cty_string =
+                    String.format("%-30s %-30s %-30s %-20s %s",
+                            cty.getName(), cty.getCountry().getName(), cty.getCountry().getRegion(), cty.getDistrict(), cty.getPopulation());
+            System.out.println(cty_string);
+        }
+    }
+
+
+}
+
