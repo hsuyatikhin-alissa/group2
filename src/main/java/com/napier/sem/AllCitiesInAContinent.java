3741afe8d9d9ac33a3b42b78d61c38988abd0791
Aye Thandar Phyo <40524297@live.napier.ac.uk>
2022-12-22 19:50:42 +0630

    Completed Issues 8, 9, 13 and 14 (Updated).

---
+++ src/main/java/com/napier/sem/AllCitiesInAContinent.java
@@ -0,0 +1,135 @@
+package com.napier.sem;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class AllCitiesInAContinent {
+
+    /**
+     * Connection to MySQL database.
+     */
+    private Connection con = null;
+
+    /**
+     * Connect to the MySQL database.
+     */
+    public void connect()
+    {
+        try
+        {
+            // Load Database driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }
+        catch (ClassNotFoundException e)
+        {
+            System.out.println("Could not load SQL driver");
+            System.exit(-1);
+        }
+
+        int retries = 10;
+        for (int i = 0; i < retries; ++i)
+        {
+            System.out.println("Connecting to database...");
+            try
+            {
+                // Wait a bit for db to start
+                Thread.sleep(30000);
+                // Connect to database
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
+                System.out.println("Successfully connected");
+                break;
+            }
+            catch (SQLException sqle)
+            {
+                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+                System.out.println(sqle.getMessage());
+            }
+            catch (InterruptedException ie)
+            {
+                System.out.println("Thread interrupted? Should not happen.");
+            }
+        }
+    }
+
+    /**
+     * Disconnect from the MySQL database.
+     */
+    public void disconnect()
+    {
+        if (con != null)
+        {
+            try
+            {
+                // Close connection
+                con.close();
+            }
+            catch (Exception e)
+            {
+                System.out.println("Error closing connection to database");
+            }
+        }
+    }
+
+    /**
+     * Get all the cities in a continent organised by largest population to smallest.
+     * @return A list of all the cities in a continent organised by largest population to smallest, or null if there is an error.
+     */
+    public ArrayList<City> getAllCitiesInAContinent()
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT city.Name, country.Name, country.Continent, city.District, city.Population "
+                            + "FROM city, country "
+                            + "WHERE city.CountryCode = country.Code "
+                            + "ORDER BY country.Continent ASC, city.Population DESC ";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Extract all the cities in a continent organised by largest population to smallest
+            ArrayList<City> cities = new ArrayList<City>();
+            while (rset.next())
+            {
+                City cty = new City();
+                Country cntry = new Country();
+                cty.setName(rset.getString(1));
+                cntry.setName(rset.getString(2));
+                cntry.setContinent(rset.getString(3));
+                cty.setDistrict(rset.getString(4));
+                cty.setPopulation(rset.getInt(5));
+                cty.setCountry(cntry);
+                cities.add(cty);
+            }
+            return cities;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get city details");
+            return null;
+        }
+    }
+
+
+    /**
+     * Prints a list of cities.
+     * @param cities The list of cities to print.
+     */
+    public void printCitiesInAContinent(ArrayList<City> cities)
+    {
+        // Print header
+        System.out.println(String.format("%-30s %-30s %-20s %-20s %s", "Name", "Country", "Continent", "District", "Population"));
+        // Loop over all cities in the list
+        for (City cty : cities)
+        {
+            String cty_string =
+                    String.format("%-30s %-30s %-20s %-20s %s",
+                            cty.getName(), cty.getCountry().getName(), cty.getCountry().getContinent(), cty.getDistrict(), cty.getPopulation());
+            System.out.println(cty_string);
+        }
+    }
+
+
+}
