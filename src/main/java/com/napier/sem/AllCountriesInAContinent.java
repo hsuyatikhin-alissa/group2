e432f4abc1c47acc07e3a13e13782745a0e2e3be
kht-40516572 <101542018+kht-40516572@users.noreply.github.com>
2022-12-23 14:08:54 +0630

    Completed and Updated All the countries in a continent organised by largest population to smallest.

--- src/main/java/com/napier/sem/AllCountriesInAContinent.java
+++ src/main/java/com/napier/sem/AllCountriesInAContinent.java
@@ -5,69 +5,15 @@ import java.util.ArrayList;
 
 public class AllCountriesInAContinent {
 
+    private String continent = "Asia";
+
     /**
      * Connection to MySQL database.
      */
-    private Connection con = null;
+    private Connection con;
 
-    /**
-     * Connect to the MySQL database.
-     */
-    public void connect()
-    {
-        try
-        {
-            // Load Database driver
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        }
-        catch (ClassNotFoundException e)
-        {
-            System.out.println("Could not load SQL driver");
-            System.exit(-1);
-        }
-
-        int retries = 10;
-        for (int i = 0; i < retries; ++i)
-        {
-            System.out.println("Connecting to database...");
-            try
-            {
-                // Wait a bit for db to start
-                Thread.sleep(30000);
-                // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
-                System.out.println("Successfully connected");
-                break;
-            }
-            catch (SQLException sqle)
-            {
-                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
-                System.out.println(sqle.getMessage());
-            }
-            catch (InterruptedException ie)
-            {
-                System.out.println("Thread interrupted? Should not happen.");
-            }
-        }
-    }
-
-    /**
-     * Disconnect from the MySQL database.
-     */
-    public void disconnect()
-    {
-        if (con != null)
-        {
-            try
-            {
-                // Close connection
-                con.close();
-            }
-            catch (Exception e)
-            {
-                System.out.println("Error closing connection to database");
-            }
-        }
+    public void setCon(Connection con) {
+        this.con = con;
     }
 
     /**
@@ -84,7 +30,8 @@ public class AllCountriesInAContinent {
             String strSelect =
                     "SELECT country.Code, country.Name, country.Continent, country.Region, country.Population, country.Capital "
                             + "FROM country "
-                            + "ORDER BY country.Continent ASC, country.Population DESC ";
+                            + "WHERE country.Continent = '" + continent + "'\n"
+                            + "ORDER BY country.Population DESC ";
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
             // Extract countries information
@@ -112,21 +59,24 @@ public class AllCountriesInAContinent {
 
 
     /**
-     * Prints a list of countries.
-     * @param countries The list of countries to print.
+     * Prints a list of countries in a continent.
+     * @param countries The list of countries in a continent to print.
      */
     public void printCountries(ArrayList<Country> countries)
     {
         // Print header
-        System.out.println(String.format("%-5s %-45s %-25s %-25s %-25s %-25s", "Code", "Name", "Continent", "Region", "Population", "Capital"));
+        System.out.println("2. All the countries in " + continent + " organised by largest population to smallest.");
+        System.out.println();
+        System.out.println(String.format("%-5s %-45s %-25s %-35s %-25s %-25s", "Code", "Name", "Continent", "Region", "Population", "Capital"));
         // Loop over all countries in the list
         for (Country cntry : countries)
         {
             String cntry_string =
-                    String.format("%-5s %-45s %-25s %-25s %-25s %-25s",
+                    String.format("%-5s %-45s %-25s %-35s %-25s %-25s",
                             cntry.getCode(), cntry.getName(), cntry.getContinent(), cntry.getRegion(), cntry.getPopulation(), cntry.getCapital());
             System.out.println(cntry_string);
         }
+        System.out.println();
     }
 
 
