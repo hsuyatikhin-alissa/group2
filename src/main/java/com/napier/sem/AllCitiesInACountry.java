5afba176329eb2ed4b592656f01896bfcbd7ed45
kht-40516572 <101542018+kht-40516572@users.noreply.github.com>
2022-12-23 14:12:26 +0630

    Completed and Updated All the cities in a country organised by largest population to smallest.

--- src/main/java/com/napier/sem/AllCitiesInACountry.java
+++ src/main/java/com/napier/sem/AllCitiesInACountry.java
@@ -5,69 +5,15 @@ import java.util.ArrayList;
 
 public class AllCitiesInACountry {
 
+    private String country = "United States";
+
     /**
      * Connection to MySQL database.
      */
-    private Connection con = null;
+    private Connection con;
 
-    /**
-     * Connect to the MySQL database.
-     */
-    public void connect()
-    {
-        try
-        {
-            // Load Database driver
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        }
-        catch (ClassNotFoundException e)
-        {
-            System.out.println("Could not load SQL driver");
-            System.exit(-1);
-        }
-
-        int retries = 10;
-        for (int i = 0; i < retries; ++i)
-        {
-            System.out.println("Connecting to database...");
-            try
-            {
-                // Wait a bit for db to start
-                Thread.sleep(30000);
-                // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
-                System.out.println("Successfully connected");
-                break;
-            }
-            catch (SQLException sqle)
-            {
-                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
-                System.out.println(sqle.getMessage());
-            }
-            catch (InterruptedException ie)
-            {
-                System.out.println("Thread interrupted? Should not happen.");
-            }
-        }
-    }
-
-    /**
-     * Disconnect from the MySQL database.
-     */
-    public void disconnect()
-    {
-        if (con != null)
-        {
-            try
-            {
-                // Close connection
-                con.close();
-            }
-            catch (Exception e)
-            {
-                System.out.println("Error closing connection to database");
-            }
-        }
+    public void setCon(Connection con) {
+        this.con = con;
     }
 
     /**
@@ -84,8 +30,9 @@ public class AllCitiesInACountry {
             String strSelect =
                     "SELECT city.Name, country.Name, country.Continent, city.District, city.Population "
                             + "FROM city, country "
-                            + "WHERE city.CountryCode = country.Code "
-                            + "ORDER BY country.Name ASC, city.Population DESC ";
+                            + "WHERE city.CountryCode = country.Code && country.Name = '" + country + "'\n"
+                            + "ORDER BY city.Population DESC ";
+
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
             // Extract cities information
@@ -120,16 +67,18 @@ public class AllCitiesInACountry {
     public void printCities(ArrayList<City> cities)
     {
         // Print header
-        System.out.println(String.format("%-30s %-30s %-20s %-20s %s", "Name", "Country", "Continent", "District", "Population"));
+        System.out.println("10. All the cities in " + country + " organised by largest population to smallest.");
+        System.out.println();
+        System.out.println(String.format("%-40s %-35s %-20s %-30s %-25s", "Name", "Country", "Continent", "District", "Population"));
         // Loop over all cities in a country in the list
         for (City cty : cities)
         {
             String cty_string =
-                    String.format("%-30s %-30s %-20s %-20s %s",
+                    String.format("%-40s %-35s %-20s %-30s %-25s",
                             cty.getName(), cty.getCountry().getName(), cty.getCountry().getContinent(), cty.getDistrict(), cty.getPopulation());
             System.out.println(cty_string);
         }
+        System.out.println();
     }
-
 
 }
