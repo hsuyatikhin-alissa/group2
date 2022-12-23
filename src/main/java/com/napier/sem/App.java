38f4bee765e1d16fc5e1f92f0990a72a15187a88
Aye Thandar Phyo <40524297@live.napier.ac.uk>
2022-12-23 14:06:00 +0630

    Completed Issues 8, 9, 13 and 14 (Updated) with separate connection file.

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -11,85 +11,55 @@ public class App
     public static void main(String[] args)
     {
 
-/*All the cities in a continent organised by largest population to smallest.*/
-
         // Create an object to call AllCitiesInAContinent Class
-        AllCitiesInAContinent ccon = new AllCitiesInAContinent();
-
+        DBConnect dbcon = new DBConnect();
         // Connect to database
-        ccon.connect();
+        dbcon.connect();
 
+
+
+        /*All the cities in a continent organised by largest population to smallest.*/
+        AllCitiesInAContinent ccon = new AllCitiesInAContinent();
+        ccon.setCon(dbcon.getCon());
         // Get all cities information in a continent
-        ArrayList<City> cities = ccon.getAllCitiesInAContinent();
-
+        ArrayList<City> citiescon = ccon.getAllCitiesInAContinent();
         // Extract all cities information in a continent
-        ccon.printCitiesInAContinent(cities);
-
-        // Disconnect from database
-        ccon.disconnect();
+        ccon.printCitiesInAContinent(citiescon);
 
 
 
-
-
-/*All the cities in a region organised by largest population to smallest.*/
-
-//        // Create an object to call AllCitiesInARegion Class
-//        AllCitiesInARegion creg = new AllCitiesInARegion();
-//
-//        // Connect to database
-//        creg.connect();
-//
-//        // Get all cities information in a region
-//        ArrayList<City> ccreg = creg.getAllCitiesInARegion();
-//
-//        // Extract all cities information in a region
-//        creg.printCitiesInARegion(ccreg);
-//
-//        // Disconnect from database
-//        creg.disconnect();
+        /*All the cities in a region organised by largest population to smallest.*/
+        AllCitiesInARegion creg = new AllCitiesInARegion();
+        creg.setCon(dbcon.getCon());
+        // Get all cities information in a continent
+        ArrayList<City> citiesreg = creg.getAllCitiesInARegion();
+        // Extract all cities information in a continent
+        creg.printCitiesInARegion(citiesreg);
 
 
 
-
-
-/*The top N populated cities in a continent where N is provided by the user.*/
-
-//        // Create an object to call TopNPopulatedCitiesInAContinent Class
-//        TopNPopulatedCitiesInAContinent tcon = new TopNPopulatedCitiesInAContinent();
-//
-//        // Connect to database
-//        tcon.connect();
-//
-//        //Get top N populated cities information in a continent
-//        ArrayList<City> tccon = tcon.getAllCities();
-//
-//        // Extract top N populated cities information in a continent
-//        tcon.printCities(tccon);
-//
-//        // Disconnect from database
-//        tcon.disconnect();
+        /*The top N populated cities in a continent where N is provided by the user.*/
+        TopNPopulatedCitiesInAContinent tccon = new TopNPopulatedCitiesInAContinent();
+        tccon.setCon(dbcon.getCon());
+        // Get all cities information in a continent
+        ArrayList<City> citiesncon = tccon.getAllCities();
+        // Extract all cities information in a continent
+        tccon.printCities(citiesncon);
 
 
 
+        /*The top N populated cities in a region where N is provided by the user.*/
+        TopNPopulatedCitiesInARegion tcreg = new TopNPopulatedCitiesInARegion();
+        tcreg.setCon(dbcon.getCon());
+        // Get all cities information in a continent
+        ArrayList<City> citiesnreg = tcreg.getAllCities();
+        // Extract all cities information in a continent
+        tcreg.printCities(citiesnreg);
 
 
-/* The top N populated cities in a region where N is provided by the user.*/
+        //Disconnect from database.
+        dbcon.disconnect();
 
-//        // Create an object to call TopNPopulatedCitiesInARegion Class
-//        TopNPopulatedCitiesInARegion treg = new TopNPopulatedCitiesInARegion();
-//
-//        // Connect to database
-//        treg.connect();
-//
-//        //Get top N populated cities information in a region
-//        ArrayList<City> tccreg = treg.getAllCities();
-//
-//        // Extract top N populated cities information in a region
-//        treg.printCities(tccreg);
-//
-//        // Disconnect from database
-//        treg.disconnect();
 
     }
 
