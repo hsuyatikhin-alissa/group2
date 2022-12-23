8d87db69874f062cf6fa7687a7ba3a3fdc5fc8d7
kht-40516572 <101542018+kht-40516572@users.noreply.github.com>
2022-12-23 14:11:32 +0630

    Completed and Updated The top N populated countries in a continent where N is provided by the user.

--- src/main/java/com/napier/sem/TopNPopulatedCountriesinacontinent.java
+++ src/main/java/com/napier/sem/TopNPopulatedCountriesinacontinent.java
@@ -5,74 +5,21 @@ import java.util.ArrayList;
 
 public class TopNPopulatedCountriesinacontinent {
 
+    private String continent = "Asia";
+    private int limit = 5;
+
     /**
      * Connection to MySQL database.
      */
-    private Connection con = null;
+    private Connection con;
 
-    /**
-     * Connect to the MySQL database.
-     */
-    public void connect()
-    {
-        try
-        {
-            // Load Database driver
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        }
-        catch (ClassNotFoundException e)
-        {
-            System.out.println("Could not load SQL driver");
-            System.exit(-1);
-        }
-
-        int retries = 10;
-        for (int i = 0; i < retries; ++i)
-        {
-            System.out.println("Connecting to database...");
-            try
-            {
-                // Wait a bit for db to start
-                Thread.sleep(30000);
-                // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
-                System.out.println("Successfully connected");
-                break;
-            }
-            catch (SQLException sqle)
-            {
-                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
-                System.out.println(sqle.getMessage());
-            }
-            catch (InterruptedException ie)
-            {
-                System.out.println("Thread interrupted? Should not happen.");
-            }
-        }
+    public void setCon(Connection con) {
+        this.con = con;
     }
 
     /**
-     * Disconnect from the MySQL database.
-     */
-    public void disconnect()
-    {
-        if (con != null)
-        {
-            try
-            {
-                // Close connection
-                con.close();
-            }
-            catch (Exception e)
-            {
-                System.out.println("Error closing connection to database");
-            }
-        }
-    }
-
-    /**
-     * Gets all Top N populated countries in the world.
-     * @return A list of all Top N populated countries in the world, or null if there is an error.
+     * Gets all Top N populated countries in a continent.
+     * @return A list of all Top N populated countries in a continent, or null if there is an error.
      */
     public ArrayList<Country> getAllCountries()
     {
@@ -84,9 +31,9 @@ public class TopNPopulatedCountriesinacontinent {
             String strSelect =
                     "SELECT country.Code, country.Name, country.Continent, country.Region, country.Population, country.Capital "
                             + "FROM country "
-                            + "WHERE country.Continent = 'Asia' "
+                            + "WHERE country.Continent = '" + continent + "'\n"
                             + "ORDER BY country.Population DESC "
-                            + "LIMIT 3 ";
+                            + "LIMIT " + limit + ";";
 
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
@@ -115,21 +62,24 @@ public class TopNPopulatedCountriesinacontinent {
 
 
     /**
-     * Prints a list of all Top N populated countries in the world.
-     * @param countries The list of all Top N populated countries in the world to print.
+     * Prints a list of all Top N populated countries in a continent.
+     * @param countries The list of all Top N populated countries in a continent to print.
      */
     public void printCountries(ArrayList<Country> countries)
     {
         // Print header
-        System.out.println(String.format("%-5s %-45s %-25s %-25s %-25s %-25s", "Code", "Name", "Continent", "Region", "Population", "Capital"));
+        System.out.println("5. The top " + limit + " populated countries in " + continent + ".");
+        System.out.println();
+        System.out.println(String.format("%-5s %-45s %-25s %-35s %-25s %-25s", "Code", "Name", "Continent", "Region", "Population", "Capital"));
         // Loop over all countries in the list
         for (Country cntry : countries)
         {
             String cntry_string =
-                    String.format("%-5s %-45s %-25s %-25s %-25s %-25s",
+                    String.format("%-5s %-45s %-25s %-35s %-25s %-25s",
                             cntry.getCode(), cntry.getName(), cntry.getContinent(), cntry.getRegion(), cntry.getPopulation(), cntry.getCapital());
             System.out.println(cntry_string);
         }
+        System.out.println();
     }
 
 
