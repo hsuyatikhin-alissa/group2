0b6f98784c326db0fcab054c9fb05a61fe258a23
suyadanawin <40542804@live.napier.ac.uk>
2022-12-23 14:02:40 +0630

    3, 6, 7, 12 issues by Suyadanawin

--- src/main/java/com/napier/sem/AllCountriesInARegion.java
+++ src/main/java/com/napier/sem/AllCountriesInARegion.java
@@ -5,73 +5,18 @@ import java.util.ArrayList;
 
 public class AllCountriesInARegion {
 
+    private String region = "Eastern Asia";
+
     /**
      * Connection to MySQL database.
      */
-    private Connection con = null;
+    private Connection con;
 
-    /**
-     * Connect to the MySQL database.
-     */
-    public void connect()
-    {
-        try
-        {
-            // Load Database driver
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        }
-        catch (ClassNotFoundException e)
-        {
-            System.out.println("Could not load SQL driver");
-            System.exit(-1);
-        }
-
-        int retries = 10;
-        for (int i = 0; i < retries; ++i)
-        {
-            System.out.println("Connecting to database...");
-            try
-            {
-                // Wait a bit for db to start
-                Thread.sleep(30000);
-                // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
-                System.out.println("Successfully connected");
-                break;
-            }
-            catch (SQLException sqle)
-            {
-                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
-                System.out.println(sqle.getMessage());
-            }
-            catch (InterruptedException ie)
-            {
-                System.out.println("Thread interrupted? Should not happen.");
-            }
-        }
+    public void setCon(Connection con) {
+        this.con = con;
     }
-
     /**
-     * Disconnect from the MySQL database.
-     */
-    public void disconnect()
-    {
-        if (con != null)
-        {
-            try
-            {
-                // Close connection
-                con.close();
-            }
-            catch (Exception e)
-            {
-                System.out.println("Error closing connection to database");
-            }
-        }
-    }
-
-    /**
-     * Gets all the current countries in a region.
+     * Gets all the countries in a region.
      * @return A list of all countries in a region, or null if there is an error.
      */
     public ArrayList<Country> getAllCountries()
@@ -82,9 +27,10 @@ public class AllCountriesInARegion {
             Statement stmt = con.createStatement();
             // Create string for SQL statement
             String strSelect =
-                    "SELECT country.Code, country.Name, country.Continent, country.Region, country.Population, country.Capital "
-                            + "FROM country "
-                            + "ORDER BY country.Population DESC ";
+                "SELECT country.Code, country.Name, country.Continent, country.Region, country.Population, country.Capital "
+                        + "FROM country "
+                        + "WHERE country.Region = '" + region + "'\n"
+                        + "ORDER BY country.Population DESC ";
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
             // Extract countries information
@@ -105,7 +51,7 @@ public class AllCountriesInARegion {
         catch (Exception e)
         {
             System.out.println(e.getMessage());
-            System.out.println("Failed to get city details");
+            System.out.println("Failed to get country details");
             return null;
         }
     }
@@ -118,6 +64,7 @@ public class AllCountriesInARegion {
     public void printCountries(ArrayList<Country> countries)
     {
         // Print header
+        System.out.println("3. All the countries in " + region + ".");
         System.out.println(String.format("%-5s %-45s %-25s %-25s %-25s %-25s", "Code", "Name", "Continent", "Region", "Population", "Capital"));
         // Loop over all countries in the list
         for (Country cntry : countries)
@@ -127,6 +74,7 @@ public class AllCountriesInARegion {
                             cntry.getCode(), cntry.getName(), cntry.getContinent(), cntry.getRegion(), cntry.getPopulation(), cntry.getCapital());
             System.out.println(cntry_string);
         }
+        System.out.println();
     }
 
 
