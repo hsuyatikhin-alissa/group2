412eaf6d64d7de0482bc222c3f327624949a9e96
hsuyatikhin-alissa <102844238+hsuyatikhin-alissa@users.noreply.github.com>
2022-12-24 20:40:00 +0630

    update main yml

---
+++ src/main/java/com/napier/sem/PopulationOfARegion.java
@@ -0,0 +1,19 @@
+package com.napier.sem;
+
+import java.sql.Connection;
+
+public class PopulationOfARegion {
+    /**
+     * Connection to MySQL database.
+     */
+    private Connection con;
+
+    public void setCon(Connection con) {
+        this.con = con;
+    }
+
+    /**
+     * The Population of A region.
+     */
+
+}
