731a4698317654f193d7de0ada623d3fe09061c7
hsuyatikhin-alissa <102844238+hsuyatikhin-alissa@users.noreply.github.com>
2022-12-24 20:51:29 +0630

    update main yml

--- src/test/java/com/napier/sem/TestAppIntegration.java
+++ src/test/java/com/napier/sem/TestAppIntegration.java
@@ -9,47 +9,10 @@ import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestAppIntegration {
-    static com.napier.sem.AllCountriesInWorld app;
-//    // Define DatabaseConnection object
-//    static DBConnect con;
-
 //    @BeforeAll
 //    static void init()
 //    {
 //        DBConnect dbcon = new DBConnect();
 //        dbcon.connect("localhost:33060", 30000);
 //    }
-    @BeforeAll
-    static void init() {
-        app = new com.napier.sem.AllCountriesInWorld();
-        app.connect("localhost:33060", 30000);
-    }
-
-
-//    @BeforeAll
-//    static void init()
-//    {
-//        app = new App();
-//        app.connect("localhost:33060", 30000);
-//
-//    }
-
-    @Test
-    void printCountries() {
-        ArrayList<Country> countries = new ArrayList<>();
-        app.printCountries(countries);
-    }
-
-//    @Test
-//    void testGetCountry()
-//    {
-//        Country cntry = app.getCountry("ABW");
-//        assertEquals(cntry.Code, "ABW");
-//        assertEquals(cntry.Name, "Aruba");
-//        assertEquals(cntry.Continent, "North America");
-//        assertEquals(cntry.Region, "Caribbean");
-//        assertEquals(cntry.Population, 103000);
-//        assertEquals(cntry.Capital, 129);
-//    }
-
 }
