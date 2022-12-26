967b09f00e50a05b3dbb5171921f48f9d6b6c730
suyadanawin <40542804@live.napier.ac.uk>
2022-12-26 16:15:03 +0630

    Integration testing

--- src/test/java/com.napier.sem/AllCountriesInARegionIntegrationTest.java
+++ src/test/java/com.napier.sem/AllCountriesInARegionIntegrationTest.java
@@ -2,7 +2,7 @@ package com.napier.sem;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,13 +15,22 @@ class AllCountriesInARegionIntegrationTest {
     static void init() {
         creg = new AllCountriesInARegion();
         db = new DBConnect();
+        db.connect("Localhost:33060", 30000);
         creg.setCon(db.getCon());
     }
-
     @Test
-    void printCountriesInARegion() {
-        ArrayList<Country> ctry = new ArrayList<Country>();
-        creg.printCountries(ctry);
+    void testCountry() throws SQLException {
+        // create array list
+        ArrayList<Country> countries = creg.getAllCountries();
+        // check array list is null
+        assertNotNull(countries);
+        /*Check expected data are correct or not*/
+        assertEquals(countries.get(0).getCode(),"CHN");
+        assertEquals(countries.get(0).getName(),"China");
+        assertEquals(countries.get(0).getContinent(),"Asia");
+        assertEquals(countries.get(0).getRegion(),"Eastern Asia");
+        assertEquals(countries.get(0).getPopulation(),1277558000);
+        assertEquals(countries.get(0).getCapital(),1891);
+        // print report
     }
-
 }
