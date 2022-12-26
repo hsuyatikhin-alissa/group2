5dc2cde1dcec8c376ccdde37887845ae6c0fe8ad
kht-40516572 <101542018+kht-40516572@users.noreply.github.com>
2022-12-26 17:08:02 +0630

    Update Integration files.

--- src/test/java/com/napier/sem/AllCountriesInAContinentIntegrationTest.java
+++ src/test/java/com/napier/sem/AllCountriesInAContinentIntegrationTest.java
@@ -2,7 +2,7 @@ package com.napier.sem;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,13 +15,24 @@ class AllCountriesInAContinentIntegrationTest {
     static void init() {
         ccon = new AllCountriesInAContinent();
         db = new DBConnect();
+        db.connect("Localhost:33060", 30000);
         ccon.setCon(db.getCon());
     }
 
-    @Test
-    void printCitiesInAContinent() {
-        ArrayList<Country> cntry = new ArrayList<Country>();
-        ccon.printCountries(cntry);
+    void testCountry() throws SQLException {
+        // create array list
+        ArrayList<Country> countries = ccon.getAllCountries();
+        // check array list is null
+        assertNotNull(countries);
+        /*Check expected data are correct or not*/
+        assertEquals(countries.get(0).getCode(),"ABW");
+        assertEquals(countries.get(0).getName(),"Aruba");
+        assertEquals(countries.get(0).getContinent(),"North America");
+        assertEquals(countries.get(0).getRegion(),"Caribbean");
+        assertEquals(countries.get(0).getPopulation(),103000);
+        assertEquals(countries.get(0).getCapital(),129);
+        // print report
     }
+
 
 }
