8c09956e967a07c5a62555214d1adcede432a580
hsuyatikhin-alissa <102844238+hsuyatikhin-alissa@users.noreply.github.com>
2022-12-26 16:28:36 +0630

    update inegration files for Integration test by HYK

--- src/test/java/com/napier/sem/AllCitiesInADistrictIntegrationTest.java
+++ src/test/java/com/napier/sem/AllCitiesInADistrictIntegrationTest.java
@@ -2,6 +2,7 @@ package com.napier.sem;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import java.sql.SQLException;
 
 import java.util.ArrayList;
 
@@ -15,13 +16,27 @@ class AllCitiesInADistrictIntegrationTest {
     static void init() {
         ctydis = new AllCitiesInADistrict();
         db = new DBConnect();
+        db.connect("Localhost:33060", 30000);
         ctydis.setCon(db.getCon());
     }
 
-    @Test
-    void printCities() {
-        ArrayList<City> cty = new ArrayList<City>();
-        ctydis.printCities(cty);
-    }
+//    @Test
+//    void printCities() {
+//        ArrayList<City> cty = new ArrayList<City>();
+//        ctydis.printCities(cty);
+//    }
 
+    @Test
+    void testTopPopulatedCitiesInACountry() throws SQLException {
+        // create array list
+        ArrayList<City> cities = ctydis.getAllCities();
+        // check array list is null
+        assertNotNull(cities);
+        /*Check expected data are correct or not*/
+        assertEquals(cities.get(0).getName(),"Amsterdam");
+        assertEquals(cities.get(0).getCountry().getName(),"Netherlands");
+        assertEquals(cities.get(0).getDistrict(),"Noord-Holland");
+        assertEquals(cities.get(0).getPopulation(),731200);
+        // print report
+    }
 }
