a7583e309648fbcb35cd5982f52fa110adfef75a
Aye Thandar Phyo <40524297@live.napier.ac.uk>
2022-12-26 15:46:43 +0630

    Updated Integration test.

--- src/test/java/com/napier/sem/TopNPopulatedCitiesInAContinentIntegrationTest.java
+++ src/test/java/com/napier/sem/TopNPopulatedCitiesInAContinentIntegrationTest.java
@@ -2,7 +2,7 @@ package com.napier.sem;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,13 +15,21 @@ class TopNPopulatedCitiesInAContinentIntegrationTest {
     static void init() {
         tcon = new TopNPopulatedCitiesInAContinent();
         db = new DBConnect();
+        db.connect("Localhost:33060", 30000);
         tcon.setCon(db.getCon());
     }
-
     @Test
-    void printCitiesInAContinent() {
-        ArrayList<City> cty = new ArrayList<City>();
-        tcon.printCities(cty);
+    void testCities() throws SQLException {
+        // create array list
+        ArrayList<City> cities = tcon.getAllCities();
+        // check array list is null
+        assertNotNull(cities);
+        /*Check expected data are correct or not*/
+        assertEquals(cities.get(0).getName(),"Mumbai (Bombay)");
+        assertEquals(cities.get(0).getCountry().getName(),"India");
+        assertEquals(cities.get(0).getCountry().getContinent(),"Asia");
+        assertEquals(cities.get(0).getDistrict(),"Maharashtra");
+        assertEquals(cities.get(0).getPopulation(),10500000);
+        // print report
     }
-
 }
--- src/test/java/com/napier/sem/TopNPopulatedCitiesInARegionIntegrationTest.java
+++ src/test/java/com/napier/sem/TopNPopulatedCitiesInARegionIntegrationTest.java
@@ -2,7 +2,7 @@ package com.napier.sem;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,13 +15,21 @@ class TopNPopulatedCitiesInARegionIntegrationTest {
     static void init() {
         treg = new TopNPopulatedCitiesInARegion();
         db = new DBConnect();
+        db.connect("Localhost:33060", 30000);
         treg.setCon(db.getCon());
     }
-
     @Test
-    void printCitiesInAContinent() {
-        ArrayList<City> cty = new ArrayList<City>();
-        treg.printCities(cty);
+    void testCities() throws SQLException {
+        // create array list
+        ArrayList<City> cities = treg.getAllCities();
+        // check array list is null
+        assertNotNull(cities);
+        /*Check expected data are correct or not*/
+        assertEquals(cities.get(0).getName(),"Seoul");
+        assertEquals(cities.get(0).getCountry().getName(),"South Korea");
+        assertEquals(cities.get(0).getCountry().getRegion(),"Eastern Asia");
+        assertEquals(cities.get(0).getDistrict(),"Seoul");
+        assertEquals(cities.get(0).getPopulation(),9981619);
+        // print report
     }
-
 }
