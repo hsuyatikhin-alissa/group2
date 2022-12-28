3a1ece9953b2843be13bc30cc9001a29ee7c2cf6
suyadanawin <40542804@live.napier.ac.uk>
2022-12-28 13:10:21 +0630

    updated files

--- src/main/java/com/napier/sem/CityReport.java
+++ src/main/java/com/napier/sem/CityReport.java
@@ -31,13 +31,72 @@ public class CityReport {
      * 7. All the cities in the world organised by largest population to smallest.
      */
 
+    /**
+     * Gets all cities in the world
+     * @return A list of all cities in the world ordered by population
+     */
+    public ArrayList<City> getAllCitiesInTheWorld()
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT city.Name, city.CountryCode, city.Population "
+                            + "FROM city, country "
+                            + "WHERE city.CountryCode = country.Code "
+                            + "ORDER BY city.Population DESC";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Extract employee information
+            ArrayList<City> cities = new ArrayList<City>();
+            while (rset.next())
+            {
+                City cty = new City();
+                cty.setName(rset.getString("Name"));
+                cty.setCountryCode(rset.getString("CountryCode"));
+                cty.setPopulation(rset.getInt("Population"));
+                cities.add(cty);
+            }
+            return cities;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get city details");
+            return null;
+        }
+    }
 
 
-
-
-
-
-
+    /**
+     * Prints all the cities
+     * @param cities to print.
+     */
+    public void printAllCitiesInTheWorld(ArrayList<City> cities)
+    {
+        // Check Cities is not null
+        if (cities == null)
+        {
+            System.out.println("No cities");
+            return;
+        }
+        // Print header
+        System.out.println("7. All the cities in the world organized by population");
+        System.out.println(String.format("%-50s %-30s %s", "Name", "CountryCode", "Population"));
+        // Loop over all cities in the list
+        for (City cty : cities)
+        {
+            if (cty == null)
+                continue;
+            String cty_string =
+                    String.format("%-50s %-30s %s",
+                            cty.getName(), cty.getCountryCode(), cty.getPopulation());
+            System.out.println(cty_string);
+        }
+        System.out.println();
+    }
 
 
     /**
@@ -305,8 +364,82 @@ public class CityReport {
      * 12. The top N populated cities in the world where N is provided by the user.
      */
 
+    /**
+     * Gets top n populated cities in the world
+     * @return A list of top n populated cities in the world
+     */
+    public ArrayList<City> getTopNPopulatedCitiesInTheWorld()
+    {
+        try
+        {
+
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT city.Name, country.Name, country.Region, city.District, city.Population "
+                            + "FROM city "
+                            + "JOIN country country "
+                            + "ON country.Code = city.CountryCode "
+                            + "ORDER BY city.Population DESC "
+                            + "LIMIT " + limit + ";";
+
+//            SELECT city.Name, country.Name, country.Continent, city.District, city.Population FROM city JOIN country country ON country.Code = city.CountryCode WHERE country.Continent = "Asia" ORDER BY Population DESC LIMIT 3;
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Extract employee information
+            ArrayList<City> cities = new ArrayList<City>();
+            while (rset.next())
+            {
+                City cty = new City();
+                Country cntry = new Country();
+                cty.setName(rset.getString(1));
+                cntry.setName(rset.getString(2));
+                cntry.setRegion(rset.getString(3));
+                cty.setDistrict(rset.getString(4));
+                cty.setPopulation(rset.getInt(5));
+                cty.setCountry(cntry);
+                cities.add(cty);
+            }
+            return cities;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get city details");
+            return null;
+        }
+    }
 
 
+    /**
+     * Prints top n populated cities in the world
+     * @param cities
+     */
+    public void printTopNPopulatedCitiesInTheWorld(ArrayList<City> cities)
+    {
+        // Check Cities is not null
+        if (cities == null)
+        {
+            System.out.println("No cities");
+            return;
+        }
+        // Print header
+        System.out.println("12. The Top " + limit + " Populated cities in the world");
+        System.out.println(String.format("%-30s %-30s %-30s %-20s %s", "Name", "Country", "Region", "District", "Population"));
+        // Loop over all cities in the list
+        for (City cty : cities)
+        {
+            if (cty == null)
+                continue;
+
+            String cty_string =
+                    String.format("%-30s %-30s %-30s %-20s %s",
+                            cty.getName(), cty.getCountry().getName(), cty.getCountry().getRegion(), cty.getDistrict(), cty.getPopulation());
+            System.out.println(cty_string);
+        }
+        System.out.println();
+    }
 
     /**
      * 13. The top N populated cities in a continent where N is provided by the user.
